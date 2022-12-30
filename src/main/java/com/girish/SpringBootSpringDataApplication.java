7dedfe1c9c324f6e065ba6e298a1220ca0029b7c
GirishKumar3763 <65503614+GirishKumar3763@users.noreply.github.com>
2022-12-30 15:19:41 +0530

    updated to war

--- src/main/java/com/girish/SpringBootSpringDataApplication.java
+++ src/main/java/com/girish/SpringBootSpringDataApplication.java
@@ -2,12 +2,19 @@ package com.girish;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootSpringDataApplication  {
+public class SpringBootSpringDataApplication extends SpringBootServletInitializer  {
+	
+	@Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(SpringBootSpringDataApplication.class);
+	 }
     
 	public static void main(String[] args) {
-		System.out.println("In main()");
+		
 		SpringApplication.run(SpringBootSpringDataApplication.class, args);
 	}
 
