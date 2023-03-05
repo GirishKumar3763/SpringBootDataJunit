c810e3e0ff170b102ad730f5a902e3d6c1edb13e
GirishKumar3763 <girishtpt517@gmail.com>
2023-03-05 12:22:29 +0530

    add docker file and docker configuration

--- src/main/java/com/girish/SpringBootSpringDataApplication.java
+++ src/main/java/com/girish/SpringBootSpringDataApplication.java
@@ -6,12 +6,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootSpringDataApplication extends SpringBootServletInitializer  {
+//public class SpringBootSpringDataApplication extends SpringBootServletInitializer  {
+public class SpringBootSpringDataApplication  {
 	
-	@Override
+	/*@Override
 	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	  return application.sources(SpringBootSpringDataApplication.class);
-	 }
+	 }*/
     
 	public static void main(String[] args) {
 		System.out.println("testing");
